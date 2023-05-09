ae4bbd5df9b732b25dca314d0e88527b1f5f0c71
Carl-Erik Kopseng <carlerik@gmail.com>
2023-05-09 19:19:27 +0200

    Default exports (only TS)
    Needs to build something as well

---
+++ lib/index.ts
@@ -0,0 +1,8 @@
+import _createDynamicImportWithRetry from "./dynamic-import-with-retry";
+import _reactLazy from './react-lazy'
+
+const defaultDynamicImportWithRetry = _createDynamicImportWithRetry(5);
+
+export const dynamicImportWithRetry = defaultDynamicImportWithRetry
+export const createDynamicImportWithRetry = _createDynamicImportWithRetry;
+export const reactLazy = _reactLazy;
