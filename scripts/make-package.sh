804f01b6e668b0d84150cd654ad17207d7bc7c39
Carl-Erik Kopseng <carlerik@gmail.com>
2023-11-28 21:31:37 +0100

    Do away with index-all.d.ts (fixes #6)

--- scripts/make-package.sh
+++ scripts/make-package.sh
@@ -8,15 +8,6 @@ if [ ! -e package.json ]; then
     exit 1
 fi
 
-cp types/index.d.ts types/index-all.d.ts
-
-echo '
-import type reactLazy from "./react-lazy.d.ts"
-
-declare module "@fatso83/retry-dynamic-import/react-lazy" {
-    export default reactLazy
-}' >> types/index-all.d.ts
-
 rm -r pkg 2>/dev/null || :
 mkdir pkg || :
 cp -a dist/* pkg/
